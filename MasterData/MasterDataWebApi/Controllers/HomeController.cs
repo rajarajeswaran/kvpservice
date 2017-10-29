30bd9e070ce1aa571cfb7699641a7b775eeb914d
rajarajeswaran <rajarajeswaran@gmail.com>
2017-10-29 21:25:37 +0000

    Hoe controller added

---
+++ MasterData/MasterDataWebApi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Http;
+
+
+namespace MasterDataWebApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public HomeController()
+        {
+        }
+
+       
+        public IHttpActionResult GetById(long id){
+            return Ok(2);
+        }
+    }
+}
